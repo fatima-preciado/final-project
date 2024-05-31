d6d7fd20e4142b2ca8346b6ebefb87b00a3636d3
conejiito <111170527+conejiito@users.noreply.github.com>
2024-05-30 21:59:02 -0700

    test

---
+++ final-project/src/pages/projects/project1.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './projects.css'
+
+function project1() {
+    return (
+        <div>
+            <h1>PANIC PAL</h1>
+                <div className="container-p">   
+                    <div className="column">
+                        <div className="disc">
+                            <p> Panic Pal is a React Native application with ChatGPT integration. The app is designed to assist individuals in managing and coping with panic attacks, which are intense episodes of fear and anxiety that can lead to symptoms such as shortness of breath, palpitations, and a sense of doom or loss of control. These episodes can significantly impact social interactions, relationships, and overall quality of life. </p>
+                        </div>
+
+                        <div className="role">
+                            <h3> ROLE </h3>
+                            
+                            <p> Front & Backend </p>
+                        </div>
+
+                        <div className="respons">
+                            <h3> RESPONSIBILITIES </h3>
+                            
+                            <p> Developed a journal feature for users to log their experiences and track their progress. Created a contact page to facilitate communication and support for users. Designed and implemented a user-friendly and intuitive login page for a seamless user experience. </p>
+                        </div>
+
+                        <div className="link">
+                            <h3> LINK </h3>
+                            <a href='https://github.com/kaitrice/panic-pal'> <p> GitHub repo </p> </a>
+                        </div>
+                    </div>
+                
+                <div className="demo">
+                    <h2>DEMO</h2>
+                </div>
+
+                </div>
+            
+        </div>
+    );
+}
+
+export default project1;
